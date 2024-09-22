df3a9342e48b13b28dcb4cb6b2711da46fbc248b
NizarMabrouk <mabrouk.mohamed.nizar@gmail.com>
2024-09-22 15:49:53 +0200

    merge de else if et inversion des conditions if on update third bloc

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -56,21 +56,19 @@ class GildedRose {
         item.sellIn --;
     }
     private void updateThirdBloc(Item item) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+        if (item.name.equals("Aged Brie")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
+        }
+        else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        } else if (item.quality > 0) {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
+            }
+            item.quality = item.quality - 1;
+        }
 
     }
 private Boolean sellIn(Item item){
